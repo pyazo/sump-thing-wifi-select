35dd773ade6edbdaa5f12d1b9f6fa270b7dace5a
Kalob Porter <kalob@kalob.net>
2019-11-13 21:50:12 -0500

    Actually display wifi, added refresh button

--- frontend/js/routes/ChooseWifi/index.jsx
+++ frontend/js/routes/ChooseWifi/index.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import List from '@material-ui/core/List';
 import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+
+import Refresh from '@material-ui/icons/Refresh';
 
 import { makeStyles } from '@material-ui/styles';
 
@@ -14,29 +17,36 @@ import style from './style';
 
 const useStyles = makeStyles(style);
 
-const data = [
-  {
-    name: 'The Promise LAN',
-    hasPassword: true,
-    strength: 4,
-  },
-  {
-    name: 'The Promise LAN 2.4GHz',
-    hasPassword: true,
-    strength: 2,
-  },
-  {
-    name: 'xfinity',
-    hasPassword: false,
-    strength: 4,
-  },
-];
-
 export default function Home() {
   const classes = useStyles();
 
   const [networks, setNetworks] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const getNetworks = async () => {
+    try {
+      setLoading(true);
+
+      const request = await fetch('http://192.168.1.1/networks');
+
+      const json = await request.json();
+
+      setLoading(false);
+
+      if (json.networks) {
+        json.networks.sort((a, b) => b.strength - a.strength);
+        setNetworks(json.networks);
+      }
+    } catch (err) {
+      setLoading(false);
+
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getNetworks();
+  }, []);
 
   return (
     <Grid container justify="center">
@@ -61,20 +71,45 @@ export default function Home() {
           </>
         )
       }
-      <Grid item xs={12}>
-        <List
-          className={classes.wifiList}
-          subheader={(
-            <ListSubheader>
-            Select a wifi network
-            </ListSubheader>
-        )}
-        >
-          {
-            data.map((d) => <WiFi {...d} />)
-          }
-        </List>
-      </Grid>
+      {
+        !loading
+        && (
+          <Grid item>
+            <IconButton onClick={getNetworks}>
+              <Refresh />
+            </IconButton>
+          </Grid>
+        )
+      }
+      {
+        networks.length > 0
+        && (
+          <Grid item xs={12}>
+            <List
+              className={classes.wifiList}
+              subheader={(
+                <ListSubheader>
+                Select a wifi network
+                </ListSubheader>
+          )}
+            >
+              {
+              networks.map((n) => <WiFi {...n} />)
+            }
+            </List>
+          </Grid>
+        )
+      }
+      {
+        !loading && networks.length === 0
+        && (
+          <Grid item xs={12}>
+            <Typography align="center" variant="subtitle1">
+              No networks found.
+            </Typography>
+          </Grid>
+        )
+      }
     </Grid>
   );
 }
